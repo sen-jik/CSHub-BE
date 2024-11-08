7158e2a569b9b14af5d61971230d22bc1811b556
jjikky <kok5417@skuniv.ac.kr>
2024-11-09 04:25:01 +0900

    feat: Swagger 테스트 겸용 User API 엔드포인트 추가 #4

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,8 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiExtraModels, ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
+import { FindUserReqDto } from './dto/req.dto';
+import { ApiGetResponse } from 'src/common/decorator/swagger.decorator';
+import { FindUserResDto } from './dto/res.dto';
 
 @ApiTags('User')
+@ApiExtraModels(FindUserReqDto, FindUserResDto)
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -10,5 +14,11 @@ export class UserController {
   @Get()
   async getUsers() {
     return await this.userService.findAll();
+  }
+
+  @ApiGetResponse(FindUserResDto)
+  @Get(':id')
+  async findOne(@Param('id') { id }: FindUserReqDto) {
+    return await this.userService.findOne(id);
   }
 }
