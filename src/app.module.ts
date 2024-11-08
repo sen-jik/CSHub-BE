8f0f78905ec4d9e722fa3ca0cfda5f712d95abb8
jjikky <kok5417@skuniv.ac.kr>
2024-11-09 02:37:25 +0900

    chore: modules 폴더 생성 및 user module 이동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { UserController } from './user/user.controller';
-import { UserModule } from './user/user.module';
+import { UserController } from './modules/user/user.controller';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [UserModule],
