f40a0f65c0b6e13059189d6b9ee9b33c0ebd79b7
jjikky <kok5417@skuniv.ac.kr>
2024-11-09 03:44:14 +0900

    feat: TypeORM 설정을 위한 ConfigService 추가 #1
    - TypeORM 설정을 위한 TypeOrmConfigService를 추가하여 PostgreSQL 설정을 기반으로 동적 TypeORM 옵션을 생성했습니다:
    - ConfigService를 사용해 환경 변수로부터 TypeORM 설정을 동적으로 로드

---
+++ src/database/typeorm-config.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { AllConfigType } from 'src/config/config.type';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService<AllConfigType>) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: this.configService.get<string>('postgres.type', {
+        infer: true,
+      }),
+      host: this.configService.get<string>('postgres.host', {
+        infer: true,
+      }),
+      port: this.configService.get<number>('postgres.port', {
+        infer: true,
+      }),
+      database: this.configService.get<string>('postgres.name', {
+        infer: true,
+      }),
+      username: this.configService.get<string>('postgres.username', {
+        infer: true,
+      }),
+      password: this.configService.get<string>('postgres.password', {
+        infer: true,
+      }),
+      synchronize: this.configService.get<boolean>('postgres.synchronize', {
+        infer: true,
+      }),
+      entities: [__dirname + '/../**/*.entity.{ts,js}'],
+      dropSchema: false,
+      keepConnectionAlive: true,
+      logging: true,
+    } as TypeOrmModuleOptions;
+  }
+}
