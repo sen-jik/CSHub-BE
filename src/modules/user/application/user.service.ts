351caea2e91077c96fcff652318dc18d9bd60572
jjikky <kok5417@skuniv.ac.kr>
2024-11-12 23:32:46 +0900

    feat: 유저 생성 로직 추가 #7

--- src/modules/user/application/user.service.ts
+++ src/modules/user/application/user.service.ts
@@ -1,12 +1,38 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../infrastructure/db/entity/user.entity';
+import { Repository } from 'typeorm';
+import { Provider } from '../domain/provider.enum';
+import { KakaoUser } from '../../auth/domain/kakao-user.type';
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
   async findAll() {
     return 'findAll';
   }
 
   async findOne(id: string) {
     return `findOne: ${id}`;
+  }
+
+  async findUserByKakaoId(kakaoId: string) {
+    const user = await this.userRepository.findOne({
+      where: { platformId: kakaoId },
+    });
+    return user;
+  }
+
+  async createUser(kakaoUser: KakaoUser) {
+    const user = new User();
+
+    // TODO : 랜던 닉네임으로 변경
+    user.nickname = kakaoUser.nickname;
+    user.profile_image = kakaoUser.profile_image;
+    user.platformId = kakaoUser.kakaoId;
+    user.provider = Provider.KAKAO;
+    return await this.userRepository.save(user);
   }
 }
