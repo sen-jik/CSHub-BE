f36a4d442d2e553849f3d7aeab0bbcb76f49d4ea
jjikky <kok5417@skuniv.ac.kr>
2024-11-12 23:31:58 +0900

    feat: 카카오 소셜 로그인 서비스 구현 #7 #8
    - 카카오 로그인 및 토큰 발급 로직 구현
    - JWT 토큰 생성 및 쿠키 설정 기능 추가
    - 카카오 인증 클라이언트 연동

--- src/modules/auth/application/auth.service.ts
+++ src/modules/auth/application/auth.service.ts
@@ -1,4 +1,72 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from '../../user/application/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { KakaoUser } from '../domain/kakao-user.type';
+import { ConfigService } from '@nestjs/config';
+import { Response } from 'express';
+import {
+  ACCESS_TOKEN_COOKIE_CONFIG,
+  REFRESH_TOKEN_COOKIE_CONFIG,
+} from '../domain/cookie.constant';
+import { KakaoAuthClient } from '../infrastructure/external/kakao/kakao-auth.client';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+    private readonly kakaoAuthClient: KakaoAuthClient,
+  ) {}
+
+  async kakaoLogin(apiKey: string, redirectUri: string, code: string) {
+    const data = await this.kakaoAuthClient.getKakaoToken(
+      apiKey,
+      redirectUri,
+      code,
+    );
+    const userInfo: KakaoUser = await this.kakaoAuthClient.getKakaoUser(
+      data.access_token,
+    );
+    return userInfo;
+  }
+
+  async findOrCreateUser(kakaoUser: KakaoUser) {
+    let user = await this.userService.findUserByKakaoId(kakaoUser.kakaoId);
+    if (!user) {
+      user = await this.userService.createUser({ ...kakaoUser });
+    }
+    return user;
+  }
+
+  async generateTokens(payload: KakaoUser) {
+    const accessToken = this.generateAccessToken(payload);
+    const refreshToken = this.generateRefreshToken(payload);
+    return { accessToken, refreshToken };
+  }
+
+  private generateAccessToken(kakaoUser: KakaoUser) {
+    const accessToken = this.jwtService.sign({
+      ...kakaoUser,
+      tokenType: 'access',
+    });
+    return accessToken;
+  }
+
+  private generateRefreshToken(kakaoUser: KakaoUser) {
+    const refreshToken = this.jwtService.sign({
+      ...kakaoUser,
+      tokenType: 'refresh',
+    });
+    return refreshToken;
+  }
+
+  setAuthCookies(
+    res: Response,
+    accessToken: string,
+    refreshToken: string,
+  ): void {
+    res.cookie('access_token', accessToken, ACCESS_TOKEN_COOKIE_CONFIG);
+    res.cookie('refresh_token', refreshToken, REFRESH_TOKEN_COOKIE_CONFIG);
+  }
+}
