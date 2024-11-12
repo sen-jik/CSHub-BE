cdcae70c019ac373260fecca3522f5b651ff7f82
jjikky <kok5417@skuniv.ac.kr>
2024-11-12 22:43:38 +0900

    feat: 유저 엔티티 및 도메인 모델 추가 #7

--- src/modules/user/domain/user.ts
+++ src/modules/user/domain/user.ts
@@ -1,1 +1,26 @@
-export class User {}
+import { Provider } from './provider.enum';
+import { Role } from './role.enum';
+
+export class User {
+  id: string;
+
+  platformId: string;
+
+  provider: Provider;
+
+  role: Role;
+
+  profile_image: string | null;
+
+  nickname: string;
+
+  total_points: number;
+
+  current_rank: number;
+
+  createdAt: Date;
+
+  updatedAt: Date;
+
+  deletedAt: Date | null;
+}
