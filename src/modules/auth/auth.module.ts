eab15c9bd5244f8cd9fa5ac4a1344f87f675cc8e
jjikky <kok5417@skuniv.ac.kr>
2024-11-12 23:31:31 +0900

    feat: Auth 및 User 모듈 기본 설정 추가 #7 #8
    - JWT, HTTP 모듈 설정 추가
    - TypeORM 설정 추가
    - 기본 의존성 주입 설정

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './presentation/auth.controller';
 import { AuthService } from './application/auth.service';
+import { HttpModule } from '@nestjs/axios';
+import { UserModule } from '../user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { KakaoAuthClient } from './infrastructure/external/kakao/kakao-auth.client';
 
 @Module({
+  imports: [
+    HttpModule,
+    UserModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          global: true,
+          secret: configService.get('auth.jwt.secret'),
+          signOptions: {
+            expiresIn: configService.get('auth.jwt.accessExpiresIn'),
+          },
+          refreshToken: {
+            expiresIn: configService.get('auth.jwt.refreshExpiresIn'),
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy, KakaoAuthClient],
   exports: [AuthService],
 })
 export class AuthModule {}
