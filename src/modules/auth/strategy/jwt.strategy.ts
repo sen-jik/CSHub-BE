5bad149397395a3c1c5ab899950fecc50fd93eb2
jjikky <kok5417@skuniv.ac.kr>
2024-11-12 23:30:12 +0900

    feat: jwt Strategy 구현 #8

---
+++ src/modules/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from './type/jwt-payload.type';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('auth.jwt.secret'),
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    if (!payload.id) {
+      throw new UnauthorizedException();
+    }
+    return payload;
+  }
+}
