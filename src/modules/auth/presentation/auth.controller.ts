218c5df94b0c57adc9ba838282789db9d1881cbb
jjikky <kok5417@skuniv.ac.kr>
2024-11-12 23:32:18 +0900

    feat: 카카오 로그인 엔드포인트 구현 #7
    - 카카오 로그인 리다이렉트 엔드포인트 추가
    - 카카오 콜백 처리 엔드포인트 구현
    - 인증 쿠키 설정 로직 추가

--- src/modules/auth/presentation/auth.controller.ts
+++ src/modules/auth/presentation/auth.controller.ts
@@ -1,4 +1,56 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  InternalServerErrorException,
+  Query,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { AuthService } from '../application/auth.service';
+import { ConfigService } from '@nestjs/config';
+import { KakaoUser } from '../domain/kakao-user.type';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  private readonly apiKey: string;
+  private readonly redirectUri: string;
+
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {
+    this.apiKey = this.configService.get<string>('auth.kakao.apiKey');
+    this.redirectUri = this.configService.get<string>('auth.kakao.redirectUri');
+  }
+
+  @Get('kakao/login')
+  @Header('Content-Type', 'text/html')
+  async kakaoRedirect(@Res() res: Response): Promise<void> {
+    const kakaoAuthorizationUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${this.apiKey}&redirect_uri=${this.redirectUri}`;
+    res.redirect(kakaoAuthorizationUrl);
+  }
+
+  @Get('kakao/callback')
+  async getKakaoInfo(@Query('code') code: string, @Res() res: Response) {
+    const kakaoUser: KakaoUser = await this.authService.kakaoLogin(
+      this.apiKey,
+      this.redirectUri,
+      code,
+    );
+
+    const user = await this.authService.findOrCreateUser(kakaoUser);
+    if (!user) {
+      throw new InternalServerErrorException('유저 생성 실패');
+    }
+    kakaoUser.id = user.id;
+
+    const { accessToken, refreshToken } =
+      await this.authService.generateTokens(kakaoUser);
+
+    this.authService.setAuthCookies(res, accessToken, refreshToken);
+
+    // TODO : 프론트 주소로 변경
+    return res.redirect('http://localhost:4000');
+  }
+}
