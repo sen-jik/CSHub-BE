32bccc271f66fb3e4dc0f5309d046caa9618c4f5
jjikky <kok5417@skuniv.ac.kr>
2024-11-25 16:42:00 +0900

    feat: 카테고리 enum 파일 추가 #20

---
+++ src/modules/quiz/domain/sub-category.enum.ts
@@ -0,0 +1,29 @@
+export enum SubCategoryEnum {
+  // FE
+  BROWSER = 'BROWSER',
+  REACT = 'REACT',
+  NEXT = 'NEXT',
+  HTML = 'HTML',
+  CSS = 'CSS',
+  FE_ETC = 'FE_ETC',
+
+  // BE
+  OS = 'OS',
+  DB = 'DB',
+  NODE = 'NODE',
+  SPRING = 'SPRING',
+  BE_ETC = 'BE_ETC',
+
+  // COMMON
+  NETWORK = 'NETWORK',
+  DATA_STRUCTURE = 'DATA_STRUCTURE',
+  ALGORITHM = 'ALGORITHM',
+  WEB = 'WEB',
+  FIT = 'FIT',
+  COMMON_ETC = 'COMMON_ETC',
+
+  // LANGUAGE
+  JAVASCRIPT = 'JAVASCRIPT',
+  TYPESCRIPT = 'TYPESCRIPT',
+  JAVA = 'JAVA',
+}
