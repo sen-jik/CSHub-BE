1f1541a4b47fe72cd03ba7a3da9c82621a59ae61
jjikky <kok5417@skuniv.ac.kr>
2024-11-28 03:34:54 +0900

    feat: 인터뷰 응답 DTO 추가 #25

--- src/modules/quiz/dto/interview.res.dto.ts
+++ src/modules/quiz/dto/interview.res.dto.ts
@@ -1,9 +1,37 @@
 import { ApiProperty } from '@nestjs/swagger';
-
+import { Interview } from '../domain/interview';
 export class CreateInterviewResDto {
   @ApiProperty({
     description: '인터뷰 ID',
     example: 1,
   })
   id: number;
+}
+
+export class GetInterviewByCategoryResDto {
+  @ApiProperty({
+    description: '메인 카테고리명',
+    example: 'COMMON',
+  })
+  mainCategoryName: string;
+
+  @ApiProperty({
+    description: '서브 카테고리명',
+    example: 'NETWORK',
+  })
+  subCategoryName: string;
+
+  @ApiProperty({
+    description: '인터뷰 목록',
+    type: [Interview],
+  })
+  interviews: Interview[];
+}
+
+export class GetAllInterviewResDto {
+  @ApiProperty({
+    description: '카테고리별 인터뷰 목록',
+    type: [GetInterviewByCategoryResDto],
+  })
+  items: GetInterviewByCategoryResDto[];
 }
