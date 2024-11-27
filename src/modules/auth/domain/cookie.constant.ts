808f96b0aba58434e6d084ceb049fd7e61aeeca5
jjikky <kok5417@skuniv.ac.kr>
2024-11-27 13:56:52 +0900

    chore: 쿠키 도메인 옵션을 STAGE마다 다르게 적용

--- src/modules/auth/domain/cookie.constant.ts
+++ src/modules/auth/domain/cookie.constant.ts
@@ -1,9 +1,18 @@
+import { ConfigService } from '@nestjs/config';
+
+const configService = new ConfigService();
+const isDevelopment = configService.get('NODE_ENV') === 'development';
+const isProduction = configService.get('NODE_ENV') === 'production';
+
+const domainConfig =
+  isDevelopment || isProduction ? { domain: '.cshub.kr' } : {};
+
 export const ACCESS_TOKEN_COOKIE_CONFIG = {
   httpOnly: true,
   secure: true,
   sameSite: 'lax',
   maxAge: 60 * 60 * 1000, // 1시간
-  domain: '.cshub.kr',
+  ...domainConfig,
 } as const;
 
 export const REFRESH_TOKEN_COOKIE_CONFIG = {
@@ -11,5 +20,5 @@ export const REFRESH_TOKEN_COOKIE_CONFIG = {
   secure: true,
   sameSite: 'lax',
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7일
-  domain: '.cshub.kr',
+  ...domainConfig,
 } as const;
