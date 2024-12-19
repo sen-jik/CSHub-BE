31683cac87d72a59c96d65ad7a720a390419895f
jjikky <kok5417@skuniv.ac.kr>
2024-12-19 11:46:48 +0900

    chore: 글로벌 파이프 암시적 형변환 활성화 #36

--- src/main.ts
+++ src/main.ts
@@ -69,6 +69,9 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
     }),
   );
 
