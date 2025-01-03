916d6e769db26166a5e42c8dda0dcbfaac7b05a2
jjikky <kok5417@skuniv.ac.kr>
2025-01-03 13:56:03 +0900

    feat: 메인 카테고리별 서브카테고리 조회 API 추가 #41
    - 메인 카테고리에 속한 서브카테고리와 각각의 인터뷰 개수를 조회하는 API 추가
    - 퍼블릭 엔드포인트로 설정

--- src/modules/category/application/catgegory.service.ts
+++ src/modules/category/application/catgegory.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { SubCategoryEnum } from '../domain/sub-category.enum';
 import { CreateSubCategoryReqDto } from '../dto/category.req.dto';
 import { SubCategory } from '../infrastructure/db/entity/sub-category.entity';
+import { MainCategoryEnum } from '../domain/main-category.enum';
 
 @Injectable()
 export class CategoryService {
@@ -24,5 +25,25 @@ export class CategoryService {
     subCategoryEntity.main_category = data.mainCategory;
 
     return this.subCategoryRepository.save(subCategoryEntity);
+  }
+
+  async findSubCategoryWithCount(mainCategory: MainCategoryEnum) {
+    const result = await this.subCategoryRepository
+      .createQueryBuilder('sub_category')
+      .leftJoin(
+        'interview',
+        'interview',
+        'interview.sub_category_id = sub_category.id',
+      )
+      .select('sub_category.name', 'name')
+      .addSelect('COUNT(interview.id)', 'interviewCount')
+      .where('sub_category.main_category = :mainCategory', { mainCategory })
+      .groupBy('sub_category.name')
+      .getRawMany();
+
+    return result.map((item) => ({
+      name: item.name,
+      interviewCount: parseInt(item.interviewCount),
+    }));
   }
 }
